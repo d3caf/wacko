0717c133f463fdddaeb10b0a84448700ec5aca21
Andrew Anguiano <andrew.anguiano@amobee.com>
2020-05-16 02:27:20 -0400

    lots of styling

--- lib/wacko_web/views/tables_view.ex
+++ lib/wacko_web/views/tables_view.ex
@@ -8,6 +8,8 @@ defmodule WackoWeb.TablesView do
     Enum.all?(players, fn {_name, %{metas: [%{ready: ready} | _]}} -> ready end)
   end
 
+  def get_turn(%Game{active_player: active_player}), do: active_player
+
   def my_turn?(%Game{active_player: active_player}, name) do
     if name == active_player, do: true, else: false
   end
@@ -18,10 +20,23 @@ defmodule WackoWeb.TablesView do
   def get_revealed(%Game{revealed: revealed}), do: revealed |> List.first()
 
   def get_rack(game, name) do
+    # TODO refactor
     get_player(game, name)
     |> Map.get(:rack)
+    |> Enum.zip(get_scale(game))
     |> Enum.with_index()
     |> Enum.reverse()
+  end
+
+  def get_scale(%Game{players: players}) do
+    case Enum.count(players) do
+      2 -> 4..40
+      3 -> 5..50
+      4 -> 6..60
+      _ -> 2..30
+    end
+    |> Enum.to_list()
+    |> Enum.take_every(Enum.count(players) + 2)
   end
 
   def get_hand(game, name) do
